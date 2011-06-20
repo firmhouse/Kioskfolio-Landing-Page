aff3a36117ca64129537c06af4ba13a5c9d58451
Michiel Sikkes <michiel.sikkes@gmail.com>
2011-06-20 10:18:10 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kioskfolio::Application.config.secret_token = '37beae228b4d03bfe2118fa2c89516741d31425fe2f23c620ba00ea45bc9a59ece3726565fe9d2efcb9853766f2b084300bb9e7a4f1a46e164ba2dbd5613a9c8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Kioskfolio::Application.config.session_store :cookie_store, key: '_kioskfolio_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Kioskfolio::Application.config.session_store :active_record_store
