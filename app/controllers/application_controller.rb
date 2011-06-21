38a79d53fa4f0a76f45fd4b140f882474bca9217
Michiel Sikkes <michiel.sikkes@gmail.com>
2011-06-21 09:26:20 +0200

    redirect to correct domain name in production

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  
+  before_filter :ensure_www
+  
+  protected
+  
+  def ensure_www
+    if Rails.env.production?
+      if request.env['HOST'] != 'www.kioskfolio.com'
+        redirect_to 'http://www.kioskfolio.com', :status => 301
+      end
+    end
+  end
 end
